1aa98b48f84187836bb1688ea3266b9fc1b03da8
Mandana <mandanakhademi88@gmail.com>
2019-04-14 19:00:48 +0100

    display country names

--- javascript.js
+++ javascript.js
@@ -32,11 +32,28 @@ showFlags = function(flags, names){
         flagImg.setAttribute('alt', 'na');
         flagImg.setAttribute('height', '32px');
         flagImg.setAttribute('width', '64px');
-        listItem.value = names[j];
+        listItem.id = names[j];
         listItem.appendChild(flagImg);
-        list[0].appendChild(listItem);
 
+        listItem.addEventListener("mouseover", eventListenerOver);
+        list[0].addEventListener("mouseout", eventListenerOut);
+
+        list[0].appendChild(listItem);
     }
+}
+
+eventListenerOver = function(event){
+    let target = event.target;
+    let countries = document.getElementsByClassName('country-name');
+    countries[0].classList.remove('hide');
+        
+    countries[0].innerText = target.parentElement.id;    
+}
+
+eventListenerOut = function(event){
+    let target = event.target;
+    let countries = document.getElementsByClassName('country-name');
+    countries[0].classList.add('hide'); 
 }
 
 window.onload = initialize;
